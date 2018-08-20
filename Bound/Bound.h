faf4c6a8d39100913812a5313881dd45bf5d7207
Muukii <muukii.app@gmail.com>
2018-08-20 19:46:42 +0900

    Initial Commit

---
+++ Bound/Bound.h
@@ -0,0 +1,19 @@
+//
+//  Bound.h
+//  Bound
+//
+//  Created by muukii on 8/20/18.
+//  Copyright © 2018 muukii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Bound.
+FOUNDATION_EXPORT double BoundVersionNumber;
+
+//! Project version string for Bound.
+FOUNDATION_EXPORT const unsigned char BoundVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Bound/PublicHeader.h>
+
+
